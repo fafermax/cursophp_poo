eff9375680dc957ca56a5aff5ea9d9985067af03
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-13 16:14:05 -0400

    atualizei

--- aula04/Caneta.php
+++ aula04/Caneta.php
@@ -1,20 +1,11 @@
 <?php 
 class Caneta {
-    public $modelo;
+    private $modelo;
+    private $cor;
     private $ponta;
+    private $tampada;
 
-    public function getModelo() {
-        return $this->modelo;
-    }
-    public function setModelo($m) {
-        $this-> modelo = $m;
-    }
-    public function getPonta() {
-        return $this->ponta;
-    }
-    public function setPonta($p) {
-        $this->ponta = $p;
-    }
+    
 
 }
 ?>
--- aula04/poo.php
+++ aula04/poo.php
@@ -12,9 +12,8 @@
         require_once "Caneta.php";
 
         $c1 = new Caneta();
-        $c1->setModelo("BIC");
-        $c1->setPonta(0.5);
-        print "Eu tenho uma caneta {$c1->getModelo()} de ponta {$c1->getPonta()}";
+        
+        print_r($c1);
     ?>
     </pre>
 </body>
