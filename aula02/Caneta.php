37e7b800229c7bd1dccbcf1036509ed5545aea1b
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-12 22:30:02 -0400

    Instancia aula02

--- aula02/Caneta.php
+++ aula02/Caneta.php
@@ -7,13 +7,18 @@ class Caneta {
     var $tampada;
 
     function rabiscar() {
-
+        if ($this->tampada == true) {
+            echo "<p>ERRO! Nao posso rabiscar!</p>";
+        } else {
+            echo "<p>Estou rabiscando!</p>";
+        }
+        
     }
     function tampar() {
-
+        $this->tampada = true;
     }
     function destampar() {
-        
+        $this->tampada = false;
     }
 }
 ?>
--- aula02/poo_01.php
+++ aula02/poo_01.php
@@ -12,9 +12,17 @@
         $c1 = new Caneta;
         $c1->cor = "Azul";
         $c1->ponta = 0.5;
-        $c1->tampada = false;
+        $c1->destampar();
 
-        var_dump($c1);
+       print_r($c1);
+       echo "<br>";
+
+        $c2 = new Caneta;
+        $c2->cor = "Preta";
+        $c2->ponta = 1.0;
+        $c2->carga = 50;
+        
+       print_r($c2);
     ?>
 </body>
 </html>
