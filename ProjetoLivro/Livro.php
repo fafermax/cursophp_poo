18e1923acc76506af69cf3de7c8f18755d71971a
Fabio Maximiliano Ferro <fafermaxx@gmail.com>
2023-03-28 21:19:29 -0400

    atualizei

--- ProjetoLivro/Livro.php
+++ ProjetoLivro/Livro.php
@@ -1,15 +1,88 @@
 <?php 
-    class Livro {
+require_once "Pessoa.php";
+require_once "Publicacao.php";
+
+    class Livro implements Publicacao {
         //Atributo
-    public $titulo;
-    public $autor;
-    public $totPaginas;
-    public $pagAtual;
-    public $aberto;
-    public $leitor;
+    private $titulo;
+    private $autor;
+    private $totPaginas;
+    private $pagAtual;
+    private $aberto;
+    private $leitor;
         //Metodos
     private function detalhes() {
         
     }
+    function __construct($titulo, $autor, $totPaginas, $leitor) {
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->totPaginas = $totPaginas;
+        $this->leitor = $leitor;
+    }
+    //Metodos Especiais
+    function getTitulo() {
+        return $this->titulo;
+    }
+    function getAutor() {
+        return $this->autor;
+    }
+    function getTotPaginas() {
+        return $this->totPaginas;
+    }
+    function getPagAtual() {
+        return $this->pagAtual;
+    }
+    function getAberto() {
+        return $this->aberto;
+    }
+    function getLeitor() {
+        return $this->leitor;
+    }
+    function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+    function setAutor($autor) {
+        $this->autor = $autor;
+    }
+    function setTotPaginas($totPaginas) {
+        $this->totPaginas = $totPaginas;
+    }
+    function setPagAtual($pagAtual) {
+        $this->pagAtual = $pagAtual;
+    }
+    function setAberto($aberto) {
+        $this->aberto = $aberto;
+    }
+    function setLeitor($leitor) {
+        $this->leitor = $leitor;
+    }
+    
+    
+    public function abrir()
+    {
+        $this->aberto = true;
+    }
+    public function avancarPag()
+    {
+        $this->pagAtual ++;
+    }
+    public function fechar()
+    {
+        $this->aberto = false;   
+    }
+    public function folhear($p)
+    {
+        if ($p > $this->totPaginas) {
+            $this->pagAtual = 0;
+        } else {
+            $this->pagAtual = $p;
+        }
+    }
+    public function voltarPag()
+    {
+       $this->pagAtual --; 
+    }
+
     }
 ?>
--- ProjetoLivro/Publicacao.php
+++ ProjetoLivro/Publicacao.php
@@ -1,62 +1,10 @@
 <?php 
-require_once "Livro.php";
+interface Publicacao {
 
-    class Publicacao {
-        
-        private $abrir;
-        private $fechar;
-        private $folhear;
-        private $avancarPag;
-        private $voltarPag;
-
-        //Metodos especiais
-
-        public function abrir() {
-
-        }
-        public function fechar() {
-
-        }
-        public function folhear() {
-
-        }
-        public function avancarPag() {
-
-        }
-        public function voltarPag() {
-
-        }
-        //Metodos especiais
-
-        function getAbrir() {
-            return $this->abrir;
-        }
-        function getFechar() {
-            return $this->fechar;
-        }
-        function getFolhear() {
-            return $this->folhear;
-        }
-        function getAvancarPag() {
-            return $this->avancarPag;
-        }
-        function getVoltarPag() {
-            return $this->voltarPag;
-        }
-        function setAbrir($abrir) {
-            $this->abrir = $abrir;
-        }
-        function setFechar($fechar) {
-            $this->fechar = $fechar;
-        }
-        function setFolhear($folhear) {
-            $this->folhear = $folhear;
-        }
-        function setAvancarPag($avancarPag) {
-            $this->avancarPag = $avancarPag;
-        }
-        function setVoltarPag($voltarPag) {
-            $this->voltarPag = $voltarPag;
-        }
+        public function abrir();
+        public function fechar();
+        public function folhear($p);
+        public function avancarPag();
+        public function voltarPag();
     }
 ?>
